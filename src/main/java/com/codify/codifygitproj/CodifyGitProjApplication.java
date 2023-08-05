fc73da6e1d730d17ed997ec4bf3e7a00ad21edf6
Александр Гузий <aleksandrguzij@MacBook-Pro-Aleksandr.local>
2023-08-05 20:24:46 +0600

    First commit

---
+++ src/main/java/com/codify/codifygitproj/CodifyGitProjApplication.java
@@ -0,0 +1,13 @@
+package com.codify.codifygitproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodifyGitProjApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodifyGitProjApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codify/codifygitproj/CodifyGitProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codify.codifygitproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodifyGitProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
